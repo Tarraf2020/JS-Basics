7e0369d591a528d24e853f25d02ed3c29252516e
Ali Tarraf <ali.tarraf2017@gmail.com>
2021-03-07 15:15:02 +0200

    step_7

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,10 @@
+function magic() {
+    var size = document.getElementById("shoe_size").value;
+    var birth = document.getElementById("year").value;
+    var n1 = size * 2;
+    var n2 = n1 + 5;
+    var n3 = n2 * 50;
+    var n4 = n3 - birth;
+    var n5 = n4 + 1766;
+alert(n5);    
+}
