fe1e7e2ca465fe3217cf8c64c1790750bae20584
Ali Tarraf <ali.tarraf2017@gmail.com>
2021-03-07 14:57:30 +0200

    step_5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,7 @@
+function multiply(){
+   var n1 = document.getElementById("first_number").value;
+   var n2 = document.getElementById("second_number").value;
+   var n = n1 * n2 ;
+   var answer = n1 + "* " + n2 + " = " + n;
+   alert(answer);
+}
