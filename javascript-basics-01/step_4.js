415435102cee7ab4f014a9bc1990228ad1c0ae7d
Ali Tarraf <ali.tarraf2017@gmail.com>
2021-03-07 14:31:56 +0200

    step 4

---
+++ javascript-basics-01/step_4.js
@@ -0,0 +1,7 @@
+function myFunction() {
+ var n = document.getElementById("name").value;
+ var s = document.getElementById("surname").value;
+ var c = document.getElementById("city").value;
+var all = "My name is : " + n + " " + s + "\n From : " + c ;
+ alert(all);
+}
