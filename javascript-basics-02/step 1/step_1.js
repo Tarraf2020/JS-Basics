8c85b6e9f0416b7730e9955a2e647c0ce423a9c6
Ali Tarraf <ali.tarraf2017@gmail.com>
2021-03-07 17:08:49 +0200

    step_1

--- javascript-basics-02/step 1/step_1.js
+++ javascript-basics-02/step 1/step_1.js
@@ -1,4 +1,12 @@
+var test = document.getElementById("image1");
+test.addEventListener("mouseover", function change(){
+  var image = document.querySelector("#image1");
+  image.src = "images/image1_2.jpg";
+} )
+
+
+/*
 function change(){
   var image = document.querySelector("#image1");
   image.src = "images/image1_2.jpg";
-}
+}*/
