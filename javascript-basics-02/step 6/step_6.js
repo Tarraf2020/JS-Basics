f3b2094bc3f9a9103e3e623efa01080b85a05394
Ali Tarraf <ali.tarraf2017@gmail.com>
2021-03-12 14:19:25 +0200

    done

--- javascript-basics-02/step 6/step_6.js
+++ javascript-basics-02/step 6/step_6.js
@@ -4,49 +4,49 @@ const img3 = document.querySelector("#image3");
 const img4 = document.querySelector("#image4");
 const img5 = document.querySelector("#image5");
 
-img1.addEventListener("mouseover", ()=>{changeImg("img1")} );
-img2.addEventListener("mouseover", ()=>{changeImg("img2")} );
-img3.addEventListener("mouseover", ()=>{changeImg("img3")} );
-img4.addEventListener("mouseover", ()=>{changeImg("img4")} );
-img5.addEventListener("mouseover", ()=>{changeImg("img5")} );
+img1.addEventListener("mouseover", ()=>{changeImg(img1)} );
+img2.addEventListener("mouseover", ()=>{changeImg(img2)} );
+img3.addEventListener("mouseover", ()=>{changeImg(img3)} );
+img4.addEventListener("mouseover", ()=>{changeImg(img4)} );
+img5.addEventListener("mouseover", ()=>{changeImg(img5)} );
 
-img1.addEventListener("mouseout", ()=>{returnImg("img1")} );
-img2.addEventListener("mouseout", ()=>{returnImg("img2")} );
-img3.addEventListener("mouseout", ()=>{returnImg("img3")} );
-img4.addEventListener("mouseout", ()=>{returnImg("img4")} );
-img5.addEventListener("mouseout", ()=>{returnImg("img5")} );
+img1.addEventListener("mouseout", ()=>{returnImg(img1)} );
+img2.addEventListener("mouseout", ()=>{returnImg(img2)} );
+img3.addEventListener("mouseout", ()=>{returnImg(img3)} );
+img4.addEventListener("mouseout", ()=>{returnImg(img4)} );
+img5.addEventListener("mouseout", ()=>{returnImg(img5)} );
 
 const  changeImg = (id) => {
-    if(id == "img1"){
+    if(id == img1){
         img1.src = "images/image1_2.jpg";
     }
-    else if(id == "img2"){
+    else if(id == img2){
         img2.src = "images/image2_2.jpg";
     }
-    else if(id == "img3"){
+    else if(id == img3){
         img3.src = "images/image3_2.jpg";
     }
-    else if(id == "img4"){
+    else if(id == img4){
         img4.src = "images/image4_2.jpg";
     }
-    else if(id == "img5")
+    else if(id == img5)
         img5.src = "images/image5_2.jpg";
 };
 
 const  returnImg = (id) => {
-    if(id == "img1"){
+    if(id == img1){
         img1.src = "images/image1.jpg";
     }
-    else if(id == "img2"){
+    else if(id == img2){
         img2.src = "images/image2.jpg";
     }
-    else if(id == "img3"){
+    else if(id == img3){
         img3.src = "images/image3.jpg";
     }
-    else if(id == "img4"){
+    else if(id == img4){
         img4.src = "images/image4.jpg";
     }
-    else if(id == "img5")
+    else if(id == img5)
         img5.src = "images/image5.jpg";
 };
 
--- javascript-basics-03/step 3/Step_3.js
+++ javascript-basics-03/step 3/Step_3.js
@@ -1,21 +1,23 @@
 const green = document.querySelector(".green");
 const red = document.querySelector(".red");
 const blue = document.querySelector(".blue");
-const text1 = document.querySelector("p");
+const text1 = document.querySelector("#text");
+
+
 
 const change1 =(id)=>{
-    if(id == "green"){
-        text1.style.color= 'green';
+    if(id == green){
+        text1.style.color= "green";
     }
-    if(id == "red"){
+    if(id == red){
         text1.style.color= 'red';
     }
-    if(id == "blue"){
+    if(id == blue){
         text1.style.color= 'blue';
     }
 }
 
 
-green.addEventListener("click", ()=>{change1("green");});
-red.addEventListener("click", ()=>{change1("red");});
-blue.addEventListener("click", ()=>{change1("blue");});
+green.addEventListener("click", ()=>{change1(green)});
+red.addEventListener("click", ()=>{change1(red)});
+blue.addEventListener("click", ()=>{change1(blue)});
